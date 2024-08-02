a9a71f2d7762ea561e229bb1def426c12bf1b4f9
BhushanGhodke <bhushanghodke1500@gmail.com>
2024-08-02 23:20:30 +0530

    perform cube on list element and filter above 50

---
+++ Stream_API_Programs/src/com/programs/Program13.java
@@ -0,0 +1,14 @@
+package com.programs;
+
+import java.util.Arrays;
+import java.util.List;
+
+//Java 8 program to perform cube on list elements and filter numbers greater than 50.
+public class Program13 {
+
+	public static void main(String[] args) {
+		 List<Integer> list = Arrays.asList(4,5,6,7,1,2,3);
+		
+		 list.stream().map(i->i*i*i).filter(result->result>50).forEach(x->System.out.println(x));
+	}
+}
