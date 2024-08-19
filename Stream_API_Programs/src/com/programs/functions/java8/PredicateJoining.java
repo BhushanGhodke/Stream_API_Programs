af7cacc5ad7367c335fb8cbc75731902ae5976d4
BhushanGhodke <bhushanghodke1500@gmail.com>
2024-08-19 17:51:29 +0530

    Predicate Joining Example

---
+++ Stream_API_Programs/src/com/programs/functions/java8/PredicateJoining.java
@@ -0,0 +1,36 @@
+package com.programs.functions.java8;
+
+import java.util.function.Predicate;
+
+//To combine multiple predicate we use predicate joining
+// 1) test == abstract method
+// 2) negate 3) and  4) or  these four method available
+
+public class PredicateJoining {
+
+	
+	public static boolean isPersonEleigibleForMembership(Person person, Predicate predicate) {
+		
+	   return predicate.test(person);
+		
+	}
+	public static void main(String[] args) {
+
+        
+		Person person= new Person("alex", 25);
+		
+		Predicate<Person> gretaerThanEighteen= (p) -> p.age > 18;
+		
+		Predicate<Person> lessThanSixty= (p) -> p.age < 60 ; 
+		
+		Predicate<Person> predicate=gretaerThanEighteen.or(lessThanSixty);
+		
+		boolean status = isPersonEleigibleForMembership(person, predicate);
+		
+		System.out.println(status);
+	}
+
+	
+
+	}
+
